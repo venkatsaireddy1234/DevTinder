05026145596b203bc17fd68bd5af0f8dad598863
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-13 17:05:47 +0530

    Added the Http methods

--- src/app.js
+++ src/app.js
@@ -4,9 +4,25 @@ const express = require('express');
 //create an app from the express module
 const app = express();
 
+
+
+app.get('/user', (req,res) =>{
+    res.send("Getting the users")
+})
+
+app.post('/user', (req,res) =>{
+    res.send({name:"venkat"})
+})
+
+app.delete("/user", (req,res) =>{
+    res.send("deleting the user")
+})
 // handle the requests for any routes
 app.use("/home", (req, res)=>{
     res.send("this is the home page")
+})
+app.use('/',(req,res) =>{
+    res.send("this is the default page")
 })
 
 // this is the request handler
