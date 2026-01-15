1921f1984d6a134d619a658bcb9b2d25b22fd00f
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-15 15:49:16 +0530

    Wrong way of using the middleware for login where middleware is not needed for the login page

--- src/app.js
+++ src/app.js
@@ -11,10 +11,13 @@ const { userMiddleware } = require('./middlewares/auth')
 // we can write the middleware in the first end point and pass it to the second route handler as below
 // or we can write the middleware globally and import it and use it in the app and it works the same way
 // using the middleware for only specific routes 
+
+// if we use the userMiddlware at the beginning then whatever the route handlers we write all the rooutes 
+// will go through these middlewares as below user login won't be needing the middleware check but it checks, thats wrong
+app.use('/user',userMiddleware)
 app.get("/user/login",(req,res)=>{
     res.send("User Logged in successfully")
 })
-app.use('/user',userMiddleware)
 app.get("/user", (req,res)=>{
     res.send("user data and checked in the above middleware")
 })
