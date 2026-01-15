9908d5dfb7130011df2654f95630cfd64dbc5f65
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-15 15:40:23 +0530

    Using the next() for the middlewares

--- src/app.js
+++ src/app.js
@@ -6,32 +6,21 @@ const app = express();
 
 
 
-//playing with the same end point with the different route handlers
-//In order to execute the multiple route handlers we need to use next() function  
-// but here once we send the response in the 15th line it stops executes the next route handler
-// so  we cannot send the multiple responses for a single end point we may skip at first route handler to see the second route handler resonse
-//what if we use next() before even sending the resonse of the first route handler?
-// It doesn't break because the first route handler executes the second route handler before 
-// responding the first route handler because of the next() and it never executes the first route handler
-//  with the response because no end point can have two responses
-// we can pass the array of route handlers as well to the same end point
-app.use("/user", 
-    (req,res, next)=>{
-    console.log("This is the first route handler")
-    // res.send("1st route handler")
-    next()
-    },
-    [(req,res, next)=>{
-    console.log("This is the second route handler")
-    res.send("2st route handler")
-    next()
-    },
-    (req, res, next) =>{
-    console.log("This is the third route handler")
-    res.send("3rd route handler")
-    next()
-    }] 
-)
+// next() is usually useful when we need to handle middlewares
+// we can write the middleware in the first end point and pass it to the second route handler as below
+app.use('/user', (req,res,next)=>{
+    const token = "token";
+    const isAuthUser = token === "token";
+    console.log("checking the authorization")
+    if(isAuthUser){
+        next()
+    }else{
+        res.status(403).send("Unauthorised User")
+    }
+})
+app.get("/user", (req,res)=>{
+    res.send("user data and checked in the above middleware")
+})
 
 
 
