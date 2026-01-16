f9567877934cbb4d8496ccd2890f42e72c575f3d
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-16 11:58:48 +0530

    Implement user signup functionality and add user model schema

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 //import the express module
 const express = require('express');
 const {connectDB} = require('./Config/dataBase');
+const User = require('./models/user');
 //create an app from the express module
 const app = express();
 
 
+app.use(express.json());
+
+app.post('/signup', async (req, res)=>{
+    const userObj = req.body;
+    console.log(userObj);
+    const user = new User(userObj);
+    try{
+        await  user.save();
+        res.send("User signed up successfully");
+    }
+    catch(err){
+        res.status(500).send("Error in signing up the user");
+    }
+})
 connectDB().then(()=>{
     console.log("DB connected successfully");
     app.listen(7777, ()=>{
