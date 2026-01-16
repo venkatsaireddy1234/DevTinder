68885855b1e4346d1519eadf5341b0878dab1440
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-16 09:57:23 +0530

    Error handlers if required body is missing

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const { userMiddleware } = require('./middlewares/auth')
 
+app.use(express.json()); // for parsing application/json
 
 // next() is usually useful when we need to handle middlewares
 // we can write the middleware in the first end point and pass it to the second route handler as below
@@ -15,13 +16,31 @@ const { userMiddleware } = require('./middlewares/auth')
 // if we use the userMiddlware at the beginning then whatever the route handlers we write all the rooutes 
 // will go through these middlewares as below user login won't be needing the middleware check but it checks, thats wrong
 app.use('/user',userMiddleware)
-app.get("/user/login",(req,res)=>{
-    res.send("User Logged in successfully")
+app.get("/user/login",(req,res,next)=>{
+    try {
+        const userEmail = req.body.email;
+        
+        if (!userEmail) {
+            throw new Error("Email is required for login")
+        }
+        
+        res.send("User Logged in successfully")
+    } catch (err) {
+        next(err)
+    }
 })
 app.get("/user", (req,res)=>{
     res.send("user data and checked in the above middleware")
 })
 
+// Global error handling middleware (add at the end, before app.listen)
+app.use((err, req, res, next) => {
+    console.log("Error caught:", err.message)
+    res.status(400).send({
+        success: false,
+        message: err.message
+    })
+})
 
 
 // accept the requests or listen to the requests 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 const userMiddleware = (req,res,next)=>{
     const token = "token";
-    const isAuthUser = token === "tokn";
+    const isAuthUser = token === "token";
     console.log("checking the authorization")
     if(!isAuthUser){
         res.status(403).send("Invalid User")
