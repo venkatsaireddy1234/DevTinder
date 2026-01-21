7fd60064195568e60cf3cadd9094bd094fa2ef45
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-21 17:25:02 +0530

    Created Send Connection Request  Api

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,26 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId:{
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: 'User'
+//Connection request schema
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    toUserId:{
-        type: mongoose.Types.ObjectId,
-        required: true,
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    status:{
-        type: String,
-        required: true,
-        enum: {
-            values :["ignored", "interested", "rejected", "accepted"],
-            message: '{VALUE} is not supported'
-        }
-    }
-},
-{timestamps: true});
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "accepted", "rejected", "interested"],
+        message: `{values} is incorrect status type`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const ConnectionRequest = mongoose.model("ConnectionRequest", connectionRequestSchema);
+//we need to create a compound index for both fromUserId and toUserId because the finding entry will be expesnsive 
+//at every time user send connection request we will check the db
+// Index : 1 meaning it will create index in ascending order
 
-module.exports = ConnectionRequest;
+connectionRequestSchema.index({fromUserId:1, toUserId:1})
+
+//before saving the req, we need to check whether the user is sending the re to himself/herself or to any other user
+
+connectionRequestSchema.pre("Save", function (next) {
+  const connectionRequest = this;
+
+  //check is fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("You Cannot send connection request to yourself");
+  }
+  next();
+});
+
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = {
+  ConnectionRequestModel,
+};
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,18 +1,70 @@
-const express = require('express')
+const express = require("express");
+const requestRouter = express.Router();
+const { userAuth } = require("../middlewares/auth");
+const { ConnectionRequestModel } = require("../models/connectionRequest");
+const User = require("../models/user");
 
-const requestRouter  = express.Router();
-const {userAuth} = require('../middlewares/auth');
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const user = req.user;
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-requestRouter.get("/sendConnectionRequest",  userAuth, async(req,res) =>{
-    try{
-        const user = req.user;
-        if(!user){
-            throw new Error("User not found");
-        }
-        res.send("Connection request sent successfully by " + user.firstName);
-    }catch(err){
-        res.status(500).send("Error in sending connection request" + err.message);
+      // check to User exist or not
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(400).json({
+          message: "User not found",
+          success: false,
+        });
+      }
+
+      // we need to check the status also, status should be only whether ignored or interested
+      const allowedStatuses = ["ignored", "interested"];
+      if (!allowedStatuses.includes(status)) {
+        throw new Error("Invalid status type:" + status);
+      }
+
+      // before sending we will check whether the request exists or not
+      // We are using $or operator to check the connection vice versa in the db
+      // otherwise it will be duplicate connection
+      const existingConnectionRequest = await ConnectionRequestModel.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      
+      if (existingConnectionRequest) {
+        throw new Error("Already sent the connection request before");
+      }
+
+      // we are sending the connection request to the toUserID and
+      // so we will call the connection Schema and send the appropriate Fields
+      const connectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+      res.status(200).json({
+        message: user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+        success: true,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: error.message,
+      });
     }
-})
- 
+  }
+);
+
+
+
 module.exports = requestRouter;
