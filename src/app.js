7f389610de9d1d8b85b9f55f7cd6ccd84ed71908
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-23 14:01:27 +0530

    Login Api has been modified

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const {connectDB} = require('./Config/dataBase');
 
 
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors')
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
@@ -12,15 +12,16 @@ const userRouter = require('./routes/user');
 //create an app from the express module
 const app = express();
 
+
+// Inorder to avoid cors issue that is it won't allow us to send one ip address to another 
+const corsOptions = {origin :"http://localhost:5173", credentials: true}
+app.use(cors(corsOptions))
 // express.json() line converts the response into json format
 app.use(express.json());
 
 // cookieParser()  parses the request for the following endpoints 
 app.use(cookieParser())
 
-// Inorder to avoid cors issue that is it won't allow us to send one ip address to another 
-const corsOptions = {origin :"http://localhost:5173", credentials: true}
-app.use(cors(corsOptions))
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,56 +1,71 @@
-const express  = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const {validateSignUp} = require('../utils/validatations');
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const { validateSignUp } = require("../utils/validatations");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
-authRouter.post('/signup', async (req, res)=>{
-    console.log("Signup route called");
-    const {firstName, lastName, email, password, age, gender, photoUrl, country, skills} = req.body;
-    try{
-     validateSignUp(req);
-     const passwordHash = await bcrypt.hash(password, 10);
+authRouter.post("/signup", async (req, res) => {
+  console.log("Signup route called");
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    age,
+    gender,
+    photoUrl,
+    country,
+    skills,
+  } = req.body;
+  try {
+    validateSignUp(req);
+    const passwordHash = await bcrypt.hash(password, 10);
     const user = new User({
-        firstName,
-        lastName,
-        email,
-        password: passwordHash,
-        age,
-        gender,
-        photoUrl,
-        country,
-        skills
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      age,
+      gender,
+      photoUrl,
+      country,
+      skills,
     });
-        await  user.save();
-        res.send("User signed up successfully");
-    }
-    catch(err){
-        res.status(500).send("Error in signing up the user" + err.message);
-    }
-})
+    await user.save();
+    res.send("User signed up successfully");
+  } catch (err) {
+    res.status(500).send("Error in signing up the user" + err.message);
+  }
+});
 
-authRouter.post('/login', async (req,res)=>{
-    const {email,password} = req.body;
-    try{
-        const user = await User.findOne({email:email})
-        if(!user){
-            throw new Error("Invalid login credentials");
-        }
-        const isPasswordValid = await user.bcryptPassword(password);
-        if(isPasswordValid){
-            const token = await user.getJWT();
-            res.cookie('token', token);
-            res.status(200).send("User logged in successfully", user);
-        }
-        else{
-            throw new Error("Invalid login credentials");
-        }
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("Invalid login credentials");
     }
-    catch(err){
-        res.status(500).send("Error in logging in the user" + err.message);
+    const isPasswordValid = await user.bcryptPassword(password);
+    if (isPasswordValid) {
+      const token = await user.getJWT();
+      res.cookie("token", token);
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: "User logged in successfully",
+          data: user,
+        });
+    } else {
+      throw new Error("Invalid login credentials");
     }
-})
-authRouter.post('/logout', (req,res)=>{
-    res.cookie('token', '', {expires: new Date()}).send("User logged out successfully");
-})
+  } catch (err) {
+    res.status(500).send("Error in logging in the user" + err.message);
+  }
+});
+authRouter.post("/logout", (req, res) => {
+  res
+    .cookie("token", "", { expires: new Date() })
+    .send("User logged out successfully");
+});
 module.exports = authRouter;
