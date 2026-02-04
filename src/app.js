0b08ac0501bfa7747adf71994d7b8f22026a737c
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-04 14:39:37 +0530

    Added the limit for the express upto till 5mb and urlencoded for the photo decoding

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ const app = express();
 const corsOptions = {origin :"http://localhost:5173", credentials: true}
 app.use(cors(corsOptions))
 // express.json() line converts the response into json format
-app.use(express.json());
+// Increase limit to allow base64 image strings from profile updates
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // cookieParser()  parses the request for the following endpoints 
 app.use(cookieParser())
@@ -36,5 +38,4 @@ connectDB().then(()=>{
 }).catch((err)=>{
     console.log("DB connection failed", err);
 })
-
 
