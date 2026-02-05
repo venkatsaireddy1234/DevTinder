a678e040e286a6a92a972aaf68aa05329d486937
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-05 16:55:41 +0530

    Changed the Format of the user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,16 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequestModel } = require("../models/connectionRequest");
 const userRouter = express.Router();
-const User = require('../models/user')
-const columns = ["firstName", "lastName", "skills", "photoUrl", "age", "about", "gender"];
+const User = require("../models/user");
+const columns = [
+  "firstName",
+  "lastName",
+  "skills",
+  "photoUrl",
+  "age",
+  "about",
+  "gender",
+];
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -43,42 +51,28 @@ userRouter.get("/user/requests/sent", userAuth, async (req, res) => {
   }
 });
 
-userRouter.get("/user/requests/accepted", userAuth, async (req, res) => {
+userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-
-    const myConnections = await ConnectionRequestModel.find({
+    const connectionRequests = await ConnectionRequestModel.find({
       $or: [
-        {
-          fromUserId: loggedInUser._id,
-          status: "accepted",
-        },
-        {
-          toUserId: loggedInUser._id,
-          status: "accepted",
-        },
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    })
-      .populate("fromUserId", columns)
-      .populate("toUserId", columns);
+    }).populate("fromUserId toUserId", columns);
 
-    //write a logic if fromuserId or touserid is loggedIn user._id then don't add it in the mycoinnections
-    const filteredConnections = myConnections.map((connection) => {
-      if (connection.fromUserId._id.equals(loggedInUser._id)) {
-        // Return only the toUser (the other person)
-        return connection.toUserId;
-      } else {
-        // Return only the fromUser (the other person)
-        return connection.fromUserId;
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() == loggedInUser._id.toString()) {
+        return row.toUserId;
       }
+      return row.fromUserId;
     });
+    console.log(data);
     res.status(200).json({
-      success: true,
-      message: "Fetched your connections successfully",
-      filteredConnections,
+      data,
     });
-  } catch (err) {
-    res.status(400).send("Something went wrong " + err.message);
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
   }
 });
 
@@ -89,11 +83,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     // we should not show the loggedInUser also
     // we will get all the connections and filter out from the users document
 
-
     const page = req.query.page || 1;
     let limit = req.query.limit || 10;
-    limit = limit >50 ? 50 : limit;
-    let skip = (page-1)*limit;
+    limit = limit > 50 ? 50 : limit;
+    let skip = (page - 1) * limit;
 
     const connectionRequests = await ConnectionRequestModel.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -109,14 +102,15 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hiddenFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(columns).skip(skip).limit(limit);
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Fetched your feed successfully",
-        allUsers,
-      });
+    })
+      .select(columns)
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json({
+      success: true,
+      message: "Fetched your feed successfully",
+      allUsers,
+    });
   } catch (err) {
     res.status(400).send("Something went wrong " + err.message);
   }
