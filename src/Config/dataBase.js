76be65ea5cb409ceda194e2e4a6ca52d5782f72f
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-11 13:05:26 +0530

    Updated the connection String

--- src/Config/dataBase.js
+++ src/Config/dataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connectionString = process.env.MONGO_URI;
+  const connectionString = process.env.MONGO_URI || "mongodb+srv://Venkat:namastenodejs@cluster0.wxmazht.mongodb.net/devTinder";
   if (!connectionString) {
     throw new Error("Missing MONGO_URI environment variable");
   }
